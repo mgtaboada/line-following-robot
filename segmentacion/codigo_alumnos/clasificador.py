269f32f80c2e7e4a8578be79883014a1948b74a8
danielfdzperez <daniel.fdz.perez@gmail.com>
2019-03-06 13:52:24 +0100

    mas eficiente

--- segmentacion/codigo_alumnos/clasificador.py
+++ segmentacion/codigo_alumnos/clasificador.py
@@ -9,6 +9,7 @@ class Clasificador:
         self.c_fondo = np.array([ 0.33413146,  0.36349363])
         self.c_linea = np.array([ 0.21104495,  0.34013866])
         self.centroids = np.array([self.c_marca,self.c_fondo,self.c_linea])
+        self.W = np.c_[np.apply_along_axis(lambda zi: zi.dot(zi)/-2,1,self.centroids),self.centroids]
     def classif_px (self,px):
         x = np.insert (px,0,1,axis=1)
         return np.argmax(x.dot (self.centroids.T))
@@ -19,9 +20,15 @@ class Clasificador:
         im =imrgb
         imn = np.rollaxis((np.rollaxis(im,2)+0.0)/np.sum(im,2),0,3)[:,:,:2]
         x,y,z = imn.shape
-        imr = np.reshape (imn,(x*y,z))
+        imr = np.reshape (imn,(-1,z))
         # cats = np.apply_along_axis (self.classif_px,1,imr)
-        d = distance_matrix (imr,self.centroids)
-        cats = np.argmin (d,axis=1)
+        d = self.distance_matrix (imr,self.centroids)
+        #cats = np.argmin (d,axis=1)
+        cats = np.argmax(d,axis=1)
         res = np.reshape (cats,(x,y))
         return res
+    def distance_matrix (self,x,y):
+        #return -2*np.dot (x,y.T) + np.sum (y**2,axis=1)# + np.sum (x**2,axis=1)
+        X = np.insert(x,0,1,axis=1)
+        return X.dot(self.W.T)
+
