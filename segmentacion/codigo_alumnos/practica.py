5a5b2306dd33fee582b9e2a80a062105b4803973
danielfdzperez <daniel.fdz.perez@gmail.com>
2019-03-07 18:16:54 +0100

    1 de 25

--- segmentacion/codigo_alumnos/practica.py
+++ segmentacion/codigo_alumnos/practica.py
@@ -17,10 +17,10 @@ cl = c.Clasificador()
 capture = cv2.VideoCapture("../videos/video1.mp4")
 
 # Ahora clasifico el video
-#n = -1
+n = -1
 while (capture.isOpened()):
     ret,img = capture.read()
-    #n+=1
+    n+=1
     if not ret:
         break
     #if ret:# and(n%15)==0:
@@ -28,13 +28,14 @@ while (capture.isOpened()):
     # obtengo la matriz de categorias
     #imgn = cv2.resize(img,(120,160))
     #img = imgn
-    #if (n%10)==0:
-    cats = cl.classif_img(img)
-    paleta = np.array([[255,0,0],[0,255,0],[0,0,255],[0,0,0]],dtype=np.uint8)
-    # ahora pinto la imagen
-    cv2.imshow("Segmentacion Euclid",cv2.cvtColor(paleta[cats],cv2.COLOR_RGB2BGR))
-    cv2.imshow("Original",img)
-    cv2.waitKey(1)
+    if (n%25)==0:
+        cats = cl.classif_img(img)
+        paleta = np.array([[255,0,0],[0,255,0],[0,0,255],[0,0,0]],dtype=np.uint8)
+        # ahora pinto la imagen
+        cv2.imshow("Segmentacion Euclid",cv2.cvtColor(paleta[cats],cv2.COLOR_RGB2BGR))
+        cv2.imshow("Original",img)
+        cv2.waitKey(1)
+        n = 0
 
     #else:
     #    break
