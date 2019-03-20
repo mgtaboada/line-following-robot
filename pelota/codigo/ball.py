beafebd6919f941ada3a44434f1aab2967b8841b
Miguel García-Mauriño Taboada <m.gtaboada@alumnos.upm.es>
2019-03-20 10:43:22 +0100

    Añado capacidad para medir la distancia a la pelota
    Además, ahora crea el video en lugar de mostrarlo en directo

--- pelota/codigo/ball.py
+++ pelota/codigo/ball.py
@@ -1,5 +1,10 @@
 #coding: utf-8
 import numpy as np
+import clasificador as c
+# Tamaño de la pelota (conocido)
+KNOWN_WIDTH = 7.0
+KNOWN_HEIGTH = 7.0
+
 def ball_square(im):
     """Devuelve las esquinas del rectángulo que inscribe a la pelota y su centro"""
     # Encontrar las coordenadas de la imagen que pertenezcan a la categoría 1 (pelota)
@@ -16,3 +21,25 @@ def ball_square(im):
     ul.reverse()
     dr.reverse()
     return tuple(ul), tuple(dr), tuple(center)
+
+def get_focal_length(im,known_distance):
+    """Calcula y devuelve la distancia focal de la camara"""
+    ul,dr,_ = ball_square(im)
+    apparent_width = abs(ul[0]-dr[0])
+    apparent_heigth = abs(ul[1]-dr[1])
+
+
+    focal_width = float(apparent_width * known_distance) / KNOWN_WIDTH
+    focal_heigth = float(apparent_heigth * known_distance) / KNOWN_HEIGTH
+
+    return np.mean([focal_heigth, focal_width])
+
+def distance_to_camera(dr,ul,focal):
+
+    apparent_width = abs(ul[0]-dr[0])
+    apparent_heigth = abs(ul[1]-dr[1])
+
+    distance_width =(KNOWN_WIDTH * focal) / apparent_width
+    distance_heigth =(KNOWN_HEIGTH * focal) / apparent_heigth
+
+    return np.mean([distance_width, distance_heigth])
--- pelota/codigo/prac_run.py
+++ pelota/codigo/prac_run.py
@@ -8,9 +8,19 @@ import clasificador as c # clasificador euclideo
 import ball # funciones para tratar la pelota
 
 cl = c.Clasificador()
+
+# Calibración de la cámara: obtener distancia focal a partir de foto con distancia conocida
+img = cv2.imread("../imagenes/30cm.png")
+cats = cl.classif_img(img)
+focal_length = ball.get_focal_length(cats,30.0) # sabemos que la foto se hizo desde 30cm
+
 # Inicio la captura de imagenes
 capture = cv2.VideoCapture("../videos/video0.mp4")
+_,img = capture.read()
 
+# creo un video nuevo como resultado
+# no he conseguido crear un .mp4
+video = cv2.VideoWriter("../videos/resultado.avi",cv2.cv.CV_FOURCC(*"XVID"),24,(img.shape[1],img.shape[0]))
 # Ahora clasifico el video
 n = -1
 while (capture.isOpened()):
@@ -19,16 +29,27 @@ while (capture.isOpened()):
     if not ret:
         break
 
-
     # obtengo la matriz de categorias
     cats = cl.classif_img(img)
     paleta = np.array([[255,0,0],[0,0,255],[255,255,0],[0,255,0]],dtype=np.uint8)
     # ahora pinto la imagen
     #cv2.imshow("Segmentacion Euclid",cv2.cvtColor(paleta[cats],cv2.COLOR_RGB2BGR))
     # Obtengo los parámetros de la pelota en la imagen
-    ul,dr,center = size.ball_square(cats)
-    cv2.rectangle(img,ul,dr,(255,0,0),2)
-    cv2.circle(img,center,3,(0,0,255),2)
-    cv2.imshow("Original",img)
-    cv2.waitKey(50)
+    ul,dr,center = ball.ball_square(cats)
+    # calculo la distancia de la pelota a la camara
+    dist = ball.distance_to_camera(dr,ul,focal_length)
 
+    #Dibujar un rectángulo alrededor de la pelota y un punto en su centro
+    cv2.rectangle(img,ul,dr,(255,0,0),2)
+    cv2.circle(img,center,3,(0,0,255),-1)
+    
+    cv2.putText(img,"{:06.3f}cm".format(dist),(img.shape[1]-250,img.shape[0]-20),cv2.FONT_HERSHEY_SIMPLEX,1.5,(0,0,0),3)
+    # Guardar el video 
+    video.write(img)
+    
+    # Mostrar el video
+    # cv2.imshow("Original",img)
+    # cv2.waitKey(50)
+cv2.destroyAllWindows()
+video.release()
+print("Terminado")
